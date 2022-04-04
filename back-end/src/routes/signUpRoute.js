193e2ce20f329245833ae089abf30fe34a78cf48
Jerson <solorsanolopez@gmail.com>
2022-04-04 16:06:49 -0700

    [Add] aws cognito to /signup route

--- back-end/src/routes/signUpRoute.js
+++ back-end/src/routes/signUpRoute.js
@@ -1,9 +1,8 @@
 import { User } from '../models/userModel.js';
-import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import config from '../config/index.js';
-import { v4 as uuid } from 'uuid';
-import { sendEmail } from '../utils/sendEmail.js';
+import { CognitoUserAttribute } from 'amazon-cognito-identity-js';
+import { awsUserPool } from '../utils/awsUserPool';
 
 export const signUpRoute = {
   path: '/api/signup',
@@ -11,64 +10,51 @@ export const signUpRoute = {
   handler: async (req, res) => {
     const { email, password } = req.body;
 
-    // check if user already exists
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(409).json({
-        error: 'User already exists',
-      });
-    }
+    const attributes = [
+      new CognitoUserAttribute({ Name: 'email', Value: email }),
+    ];
 
-    // encrypt password
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    // create the e-mail verification
-    const verificationString = uuid();
-
-    const startingInfo = {
-      hairColor: '',
-      favoriteFood: '',
-      bio: '',
-    };
-
-    // create new user
-    const newUser = new User({
+    awsUserPool.signUp(
       email,
-      passwordHash,
-      info: startingInfo,
-      isVerified: false,
-      verificationString: verificationString,
-    });
-
-    // save user
-    const savedUser = await newUser.save();
-
-    // send verification email
-    try {
-      await sendEmail({
-        to: email,
-        from: 'jersonquinterolopez@gmail.com',
-        subject: 'Please Verify your email',
-        text: `Thanks for signing up! Please verify your email by clicking this link: http://localhost:3000/verify-email/${verificationString}`,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
-    }
-
-    // create token
-    jwt.sign(
-      { _id: savedUser._id },
-      config.authJwtSecret,
-      {
-        expiresIn: '2d',
-      },
-      (err, token) => {
+      password,
+      attributes,
+      null,
+      async (err, awsResult) => {
         if (err) {
-          return res.status(500).send(err);
+          console.log(err);
+          return res.status(500).json({ msg: 'Unable to signup user' });
         }
-        res.status(200).json({ token });
+
+        const startingInfo = {
+          hairColor: '',
+          favoriteFood: '',
+          bio: '',
+        };
+
+        const newUser = new User({
+          email,
+          info: startingInfo,
+        });
+
+        const savedUser = await newUser.save();
+        const { _id } = savedUser;
+
+        jwt.sign(
+          {
+            id: _id,
+            isVerified: false,
+            email,
+            info: startingInfo,
+          },
+          config.authJwtSecret,
+          {
+            expiresIn: '2d',
+          },
+          (err, token) => {
+            if (err) return res.status(500);
+            res.status(200).json({ token });
+          }
+        );
       }
     );
   },
