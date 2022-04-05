ec641019d9cc61ff89c7952671e9d628d3bf1351
Jerson <solorsanolopez@gmail.com>
2022-04-04 20:43:25 -0700

    [Refactor]c reset password route

--- back-end/src/routes/resetPasswordRoute.js
+++ back-end/src/routes/resetPasswordRoute.js
@@ -1,29 +1,24 @@
-import bcrypt from 'bcrypt';
-import { User } from '../models/userModel.js';
+import { CognitoUser } from 'amazon-cognito-identity-js';
+import { awsUserPool } from '../utils/awsUserPool.js';
 
 export const resetPasswordRoute = {
   path: '/api/users/:passwordResetCode/reset-password',
   method: 'put',
   handler: async (req, res) => {
-    try {
-      const { passwordResetCode } = req.params;
-      const { newPassword } = req.body;
+    const { passwordResetCode } = req.params;
+    const { email, newPassword } = req.body;
 
-      const salt = await bcrypt.genSalt();
-      const newPasswordHash = await bcrypt.hash(newPassword, salt);
-
-      const user = await User.findOne({ passwordResetCode: passwordResetCode });
-
-      if (!user) return res.status(400);
-
-      user.passwordHash = newPasswordHash;
-      user.passwordResetCode = '';
-
-      await user.save();
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500);
-    }
+    new CognitoUser({ Username: email, Pool: awsUserPool }).confirmPassword(
+      passwordResetCode,
+      newPassword,
+      {
+        onSuccess: () => {
+          return res.status(200);
+        },
+        onFailure: () => {
+          return res.status(401);
+        },
+      }
+    );
   },
 };
