efcb0acff31f80d320b534bfd2ffdd6743d25aee
Jerson <solorsanolopez@gmail.com>
2022-04-04 18:58:50 -0700

    [Implement] amazon cognito email verification route

--- back-end/src/routes/verifyEmailRoute.js
+++ back-end/src/routes/verifyEmailRoute.js
@@ -1,29 +1,39 @@
 import jwt from 'jsonwebtoken';
 import config from '../config/index.js';
 import { User } from '../models/userModel.js';
+import { CognitoUser } from 'amazon-cognito-identity-js';
+import { awsUserPool } from '../utils/awsUserPool.js';
 
 export const verifyEmailRoute = {
   path: '/api/verify-email',
   method: 'put',
   handler: async (req, res) => {
-    const { verificationString } = req.body;
-    const result = await User.findOne({ verificationString });
+    const { email, verificationString } = req.body;
 
-    if (!result)
-      return res
-        .status(401)
-        .json({ message: 'The email verification code is incorrect' });
+    new CognitoUser({ Username: email, Pool: awsUserPool }).confirmRegistration(
+      verificationString,
+      true,
+      async (err) => {
+        if (err)
+          return res
+            .status(401)
+            .json({ msg: 'The email verification is incorrect' });
 
-    const { _id: id, email, info } = result;
-    await User.findByIdAndUpdate(id, { isVerified: true });
+        const user = await User.findOne({ email });
+        user.isVerified = true;
 
-    jwt.sign(
-      { id, email, isVerified: true, info },
-      config.authJwtSecret,
-      { expiresIn: '2d' },
-      (err, token) => {
-        if (err) return res.status(500);
-        res.status(200).json({ token });
+        const verifiedUser = await user.save();
+        const { _id: id, info } = verifiedUser;
+
+        jwt.sign(
+          { id, email, isVerified: true, info },
+          config.authJwtSecret,
+          { expiresIn: '2d' },
+          (err, token) => {
+            if (err) return res.status(500);
+            res.status(200).json({ token });
+          }
+        );
       }
     );
   },
